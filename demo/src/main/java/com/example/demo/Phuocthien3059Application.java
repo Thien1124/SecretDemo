5a5ba719b7adf053fb43c5ffb97e1a7a9a606a49
Nguyễn Phước Thiện <npthien124@gmail.com>
2026-03-25 09:30:36 +0700

    [feat: ok]

---
+++ demo/src/main/java/com/example/demo/Phuocthien3059Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phuocthien3059Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phuocthien3059Application.class, args);
+	}
+
+}
